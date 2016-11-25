a51471b9c356fb287dfc908f4545c5a6edc7667b
Stephane Nicoll <snicoll@pivotal.io>
2016-11-25 11:06:17 +0100

    Upgrade to Spring Boot 1.5.0 snap

--- spring-boot-maven-tests-misc/src/it/jar-devtools-default/verify.groovy
+++ spring-boot-maven-tests-misc/src/it/jar-devtools-default/verify.groovy
@@ -1,11 +1,12 @@
 import java.io.*;
 import org.springframework.boot.maven.*;
 
-File f = new File( basedir, "target/jar-exclude-devtools-0.0.1.BUILD-SNAPSHOT.jar");
+File f = new File( basedir, "target/jar-devtools-default-0.0.1.BUILD-SNAPSHOT.jar");
 new Verify.JarArchiveVerification(f, Verify.SAMPLE_APP) {
 	@Override
 	protected void verifyZipEntries(Verify.ArchiveVerifier verifier) throws Exception {
 		super.verifyZipEntries(verifier)
-		verifier.assertHasNoEntryNameStartingWith("lib/spring-boot-devtools")
+		verifier.assertHasNoEntryNameStartingWith("BOOT-INF/lib/spring-boot-devtools")
 	}
 }.verify();
+
